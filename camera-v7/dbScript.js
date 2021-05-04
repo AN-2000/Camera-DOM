e0ed46f016a54686965e2d980de352be4c5e8d1e
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-05-04 12:18:00 +0530

    minor bug fix in v6

--- camera-v7/dbScript.js
+++ camera-v7/dbScript.js
@@ -16,27 +16,20 @@ request.onupgradeneeded = function (e) {
 
 function addMediaToGallery(data, type) {
   let tx = db.transaction("gallery", "readwrite");
-   let gallery = tx.objectStore("gallery");
+  let gallery = tx.objectStore("gallery");
   gallery.add({ mId: Date.now(), type, media: data });
 }
 
-// C2
 function viewMedia() {
   let body = document.querySelector("body");
-  // open up a transaction to read from gallery objectStore
   let tx = db.transaction("gallery", "readonly");
   let gallery = tx.objectStore("gallery");
-  // Create a cursor
   let req = gallery.openCursor();
   req.onsuccess = function (e) {
     let cursor = req.result;
-    // Using the cursor get all objects stored in the store one by one
     if (cursor) {
-      //Check if they are of type video or image
       if (cursor.value.type == "video") {
-        //if they are of type video create a video element and render media to screen with delete and download buttons(in this version buttons will not work)
         let vidContainer = document.createElement("div");
-        // This id is required later to perform delete
         vidContainer.setAttribute("data-mId", cursor.value.mId);
         vidContainer.classList.add("gallery-vid-container");
         let video = document.createElement("video");
@@ -45,33 +38,38 @@ function viewMedia() {
         let deleteBtn = document.createElement("button");
         deleteBtn.classList.add("gallery-delete-button");
         deleteBtn.innerText = "Delete";
+        // C3.2
+        deleteBtn.addEventListener("click", deleteBtnHandler);
         let downloadBtn = document.createElement("button");
         downloadBtn.classList.add("gallery-download-button");
         downloadBtn.innerText = "Download";
+        // C4.2
+        downloadBtn.addEventListener("click", donwloadBtnHandler);
         vidContainer.appendChild(deleteBtn);
         vidContainer.appendChild(downloadBtn);
 
-        //if you dont give video controls you cannot have autoplay
         video.autoplay = true;
         video.controls = true;
         video.loop = true;
-        // This creates a url of a blob object
         video.src = window.URL.createObjectURL(cursor.value.media);
         body.appendChild(vidContainer);
       } else {
-        // similarly if they are image create img element and render them
         let imgContainer = document.createElement("div");
         imgContainer.setAttribute("data-mId", cursor.value.mId);
-        imgContainer.classList.add("gallery-img-container");
+        imgContainer.classList.add("gallery-img-container")
         let img = document.createElement("img");
         img.src = cursor.value.media;
         imgContainer.appendChild(img);
         let deleteBtn = document.createElement("button");
         deleteBtn.classList.add("gallery-delete-button");
         deleteBtn.innerText = "Delete";
+        // C3.3
+        deleteBtn.addEventListener("click", deleteBtnHandler);
         let downloadBtn = document.createElement("button");
         downloadBtn.classList.add("gallery-download-button");
         downloadBtn.innerText = "Download";
+        // C4.3
+        downloadBtn.addEventListener("click", donwloadBtnHandler);
         imgContainer.appendChild(deleteBtn);
         imgContainer.appendChild(downloadBtn);
         body.appendChild(imgContainer);
@@ -79,4 +77,34 @@ function viewMedia() {
       cursor.continue();
     }
   };
+}
+
+// C2
+function deleteMediaFromGallery(mId) {
+  let tx = db.transaction("gallery", "readwrite");
+  let gallery = tx.objectStore("gallery");
+  console.log(mId);
+  //Typecasting to a number is imp because we have stored Date.now() which gives us a number
+  gallery.delete(Number(mId));
+}
+
+// C3.1
+function deleteBtnHandler(e) {
+  let mId = e.currentTarget.parentNode.getAttribute("data-mId");
+  deleteMediaFromGallery(mId);
+  e.currentTarget.parentNode.remove();
+}
+
+// C4.1
+function donwloadBtnHandler(e) {
+  let a = document.createElement("a");
+  a.href = e.currentTarget.parentNode.children[0].src;
+  // checks if the element is video or img so that we can define the extension 
+  if (e.currentTarget.parentNode.children[0].nodeName == "IMG") {
+    a.download = "image.png";
+  } else {
+    a.download = "video.mp4";
+  }
+  a.click()
+  a.remove()
 }
